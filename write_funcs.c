4c7c06a716c47d607602b3f6416e6fae2bab439c
root <githinjibrian5@gmail.com>
2022-04-19 16:22:35 -0700

    debugging

--- write_funcs.c
+++ write_funcs.c
@@ -1,42 +1,78 @@
 #include "main.h"
-#include <unistd.h>
+#include <stdio.h>
 
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- * Description: _putchar uses a local buffer of 1024 to call write
- * as little as possible
+ * rev_string - reverses a string in place
+ *
+ * @s: string to reverse
+ * Return: A pointer to a character
  */
-int _putchar(char c)
+char *rev_string(char *s)
 {
-	static char buf[1024];
-	static int i;
+	int len;
+	int head;
+	char tmp;
+	char *dest;
 
-	if (c == -1 || i >= 1024)
+	for (len = 0; s[len] != '\0'; len++)
+	{}
+
+	dest = malloc(sizeof(char) * len + 1);
+	if (dest == NULL)
+		return (NULL);
+
+	_memcpy(dest, s, len);
+	for (head = 0; head < len; head++, len--)
 	{
-		write(1, &buf, i);
-		i = 0;
+		tmp = dest[len - 1];
+		dest[len - 1] = dest[head];
+		dest[head] = tmp;
 	}
-	if (c != -1)
-	{
-		buf[i] = c;
-		i++;
-	}
-	return (1);
+	return (dest);
 }
 
 /**
- * _puts - prints a string to stdout
- * @str: pointer to the string to print
- * Return: number of chars written
+ * write_base - sends characters to be written on standard output
+ * @str: String to parse
  */
-int _puts(char *str)
+void write_base(char *str)
 {
-	register int i;
+	int i;
 
 	for (i = 0; str[i] != '\0'; i++)
-	_putchar(str[i]);
+	_write_char(str[i]);
+}
+
+/**
+ * base_len - Calculates the length for an octal number
+ * @num: The number for which the length is being calculated
+ * @base: Base to be calculated by
+ * Return: An integer representing the length of a number
+ */
+unsigned int base_len(unsigned int num, int base)
+{
+	unsigned int i;
+
+	for (i = 0; num > 0; i++)
+	{
+		num = num / base;
+	}
 	return (i);
+}
+
+/**
+ * _memcpy - copy memory area
+ * @dest: Destination for copying
+ * @src: Source to copy from
+ * @n: The number of bytes to copy
+ * Return: The _memcpy() function returns a pointer to dest.
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	dest[i] = src[i];
+	dest[i] = '\0';
+	return (dest);
 }
